9bf9d0b7e8089034a4bfe1f12780fa1459782619
AjayScalelot <ajay.sclelot@gmail.com>
2022-12-20 18:42:52 +0530

    Notification Gallary Slider

--- src/component/other/modal/GalleryImageAndVideoPreview.js
+++ src/component/other/modal/GalleryImageAndVideoPreview.js
@@ -3,21 +3,16 @@ import bigDishImage from "../../../assest/images/big-dish.png";
 import dish1Image from "../../../assest/images/dish-1.png";
 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Thumbs } from "swiper";
-import 'swiper/css/bundle';
+import { FreeMode, Navigation, Thumbs } from "swiper";
 import "swiper/css";
-
-// import "swiper/css/free-mode";
+import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
 import { s3Url } from "../../../config";
 
-// Swiper thumb example
-// https://codesandbox.io/s/pfhz3h?file=/src/App.jsx:2237-2250
 
 function GalleryImageAndVideoPreview({ handleClose, data }) {
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
-  const [activeThumb, setActiveThumb] = useState();
 
   return (
     <div className="fixed inset-0 w-full h-full bg-[rgba(0,0,0,0.6)] flex z-50">
@@ -33,16 +28,17 @@ function GalleryImageAndVideoPreview({ handleClose, data }) {
           <h2 className="text-white">Sweet Love Catering</h2>
           <p className="text-lg text-white font-normal">Catering</p>
         </div>
-        <div className="swiper-container gallery-swippers gallery-top gallery-img relative">
+        <div className="swiper-container gallery-top gallery-img relative">
           <Swiper
             style={{
               "--swiper-navigation-color": "#fff",
               "--swiper-pagination-color": "#fff",
             }}
+            centeredSlides={false}
             spaceBetween={10}
             navigation={true}
+            modules={[FreeMode, Navigation, Thumbs]}
             thumbs={{ swiper: thumbsSwiper && !thumbsSwiper.destroyed ? thumbsSwiper : null }}
-            modules={[Navigation, Thumbs]}
           >
             {data.map((e, i) => (
               <SwiperSlide key={i}>
@@ -52,34 +48,36 @@ function GalleryImageAndVideoPreview({ handleClose, data }) {
                       <img src={s3Url + "/" + e.url} alt="big-dish" className="w-full" /> :
                       <video width="100%" src={s3Url + "/" + e?.url} alt="no video" controls allowFullScreen></video>
                     )}
-
                   </div>
                 </div>
               </SwiperSlide>
             ))}
           </Swiper>
-          <div className="swiper-container gallery-thumbs bg-black">
-            <Swiper
-              onSwiper={setThumbsSwiper}
-              spaceBetween={10}
-              slidesPerView={4}
-              freeMode={true}
-              watchSlidesProgress={true}
-              modules={[Navigation, Thumbs]}
-              className="mySwiper"
-            >-
-              {data.map((e, i) => (
-                <SwiperSlide key={i}>
-                  <div className="rounded-md overflow-hidden">
-                    {(e.type === "photo" ?
-                      <img src={s3Url + "/" + e.url} alt="/dish-1" /> :
-                      <video width="100%" src={s3Url + "/" + e?.url} alt="no video" controls allowFullScreen></video>
-                    )}
-                  </div>
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          </div>
+        </div>
+        <div className="swiper-container sw2 gallery-thumbs bg-black">
+          <Swiper
+            onSwiper={setThumbsSwiper}
+            centeredSlides={true}
+            slidesPerView={'auto'}
+            touchRatio={0.2}
+            freeMode={true}
+            slideToClickedSlide={true}
+            loopedSlides={4}
+            spaceBetween={10}
+            watchSlidesProgress={true}
+            modules={[FreeMode, Navigation, Thumbs]}
+          >-
+            {data.map((e, i) => (
+              <SwiperSlide key={i}>
+                <div className="rounded-md overflow-hidden w-28 h-28 mx-auto">
+                  {(e.type === "photo" ?
+                    <img src={s3Url + "/" + e.url} alt="big-dish" className="w-full h-full" /> :
+                    <video width="100%" height="100%" src={s3Url + "/" + e?.url} alt="no video" controls allowFullScreen></video>
+                  )}
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
         </div>
 
       </div>
