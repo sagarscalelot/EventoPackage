dc0d0291a65af2acb233bf0510c4121abfcba652
Dixit Lukhi <lukhidixit149@gmail.com>
2022-12-21 12:46:23 +0530

    in overview items remain, gallery design remain

--- src/component/events/EventTermsAndConditions.js
+++ src/component/events/EventTermsAndConditions.js
@@ -120,27 +120,28 @@ function EventTermsAndConditions() {
                   <h3>Terms and Conditions</h3>
             
                  <CKEditor
-                    config={editorConfig}
+                    // config={editorConfig}
                     editor={ ClassicEditor }
-                    data={terms}
-                    onReady={ editor => {
-                      // You can store the "editor" and use when it is needed.
-                        // console.log( 'Editor is ready to use!', editor );
-                    } }
+                    // data={terms}
+                    // onReady={ editor => {
+                    //   // You can store the "editor" and use when it is needed.
+                    //     // console.log( 'Editor is ready to use!', editor );
+                    // } }
                     onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        setTerms(editor.getData());
+                        // const data = editor.getData();
+                        setTerms((editor.getData()));
                         // console.log( { event, editor, data } );
                     } }
-                    onBlur={ ( event, editor ) => {
-                        // console.log( 'Blur.', editor );
-                    } }
-                    onFocus={ ( event, editor ) => {
-                        // console.log( 'Focus.', editor );
-                    } }
+                    // onBlur={ ( event, editor ) => {
+                    //     // console.log( 'Blur.', editor );
+                    // } }
+                    // onFocus={ ( event, editor ) => {
+                    //     // console.log( 'Focus.', editor );
+                    // } }
                 />
 
                 </div>
+                {terms}
                 <div className="w-full">
                   <h3>Social Media</h3>
                   <div className="-mx-2">
