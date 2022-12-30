442ed88c4940970b4f2226ff8d631a472bd01829
Ajay Dabhi <ajay.scalelot@gmail.com>
2022-12-30 14:32:07 +0530

    pull on new changes

--- src/component/events/DashboardEventCategoryItem.js
+++ src/component/events/DashboardEventCategoryItem.js
@@ -10,35 +10,35 @@ import { baseUrl } from '../../config';
 
 
 
- 
- const Star = ({ratings}) => {
+
+const Star = ({ ratings }) => {
 	const numberRating = Number(ratings);
-	console.log("numberRating",numberRating);
-  const ratingStar = Array.from({ length: 5 }, (elem, index) => {
-    let number = index + 0.5;
-    return (
-      <span key={index}>
-        {numberRating >= index + 1 ? (
-          <i className="icon-fillStar text-sm"></i>
-        ) : numberRating >= number ? (
-          <i className="icon-half-star text-sm"></i>
-        ) : (
-          <i className="icon-star text-sm"></i>
-        )}
-      </span>
-    );
-  });
-	 return (
+	console.log("numberRating", numberRating);
+	const ratingStar = Array.from({ length: 5 }, (elem, index) => {
+		let number = index + 0.5;
+		return (
+			<span key={index}>
+				{numberRating >= index + 1 ? (
+					<i className="icon-fillStar text-sm"></i>
+				) : numberRating >= number ? (
+					<i className="icon-half-star text-sm"></i>
+				) : (
+					<i className="icon-star text-sm"></i>
+				)}
+			</span>
+		);
+	});
+	return (
 		<div>{ratingStar}</div>
-	 )
- };
- 
-
- 
+	)
+};
 
 
 
-function  DashboardEventCategoryItem({ data, handleClick }) {
+
+
+
+function DashboardEventCategoryItem({ data, handleClick }) {
 
 
 	const dispatch = useDispatch();
@@ -76,22 +76,22 @@ function  DashboardEventCategoryItem({ data, handleClick }) {
 
 
 	return (
-		<div className="w-full flex items-center ">			
-				<div className="flex space-x-5 w-full p-4 pr-7 bg-white rounded">
-					<div className="max-w-xs h-[200px] w-full">
+		<div className="w-full flex items-center ">
+			<div className="flex space-x-5 w-full p-4 pr-7 bg-white rounded">
+				<div className="max-w-xs h-[200px] w-full">
 					<Link to={`../../event-view/${eventType}`} className="" onClick={() => { localStorage.setItem("eventId", data?._id) }} >
 						<img className="object-cover w-full h-full" src={(data && data.aboutplace && data.aboutplace.banner && data.aboutplace.banner != '') ? (s3Url + "/" + data.aboutplace.banner) : (data && data.personaldetail && data.personaldetail.banner && data.personaldetail.banner !== '') ? (s3Url + "/" + data.personaldetail.banner) : bannerPreview} alt="images" /></Link>
-					</div>
-					<div className="w-full">
-						<div className="flex justify-between border-b-2 pb-4">
-							<div className="">
-								<span className="text-sm text-white bg-spiroDiscoBall px-3 py-1">{data?.event_category.category_name}</span>
-								<h2 className="text-japaneseIndigo pt-5">{data?.display_name}</h2>
-								<div className="text-sm text-quicksilver pt-3"><i className="icon-fill-location mr-3"></i>{(data?.capacity?.address && data?.capacity?.address !== "") || (data?.personaldetail?.area && data?.personaldetail?.area !=="" && data?.personaldetail?.city && data?.personaldetail?.city !=="" && data?.personaldetail?.state && data?.personaldetail?.state !=="" ?  data?.personaldetail?.area + ","+ data?.personaldetail?.city+"," +  data?.personaldetail?.state : "")}
-</div>
+				</div>
+				<div className="w-full">
+					<div className="flex justify-between border-b-2 pb-4">
+						<div className="">
+							<span className="text-sm text-white bg-spiroDiscoBall px-3 py-1">{data?.event_category.category_name}</span>
+							<h2 className="text-japaneseIndigo pt-5">{data?.display_name}</h2>
+							<div className="text-sm text-quicksilver pt-3"><i className="icon-fill-location mr-3"></i>{(data?.capacity?.address && data?.capacity?.address !== "") || (data?.personaldetail?.area && data?.personaldetail?.area !== "" && data?.personaldetail?.city && data?.personaldetail?.city !== "" && data?.personaldetail?.state && data?.personaldetail?.state !== "" ? data?.personaldetail?.area + "," + data?.personaldetail?.city + "," + data?.personaldetail?.state : "")}
 							</div>
-							<div className="">
-								<div className="flex items-center">
+						</div>
+						<div className="">
+							<div className="flex items-center">
 
 								{/* {console.log("check : ", data?.display_name, data?.is_live)} */}
 
@@ -102,39 +102,37 @@ function  DashboardEventCategoryItem({ data, handleClick }) {
 									// onClick={() => singleEventlive(data?._id)}
 									onChange={() => toggleEvent(data)}
 
-									/>
-									<label htmlFor="">
-										<h3>Live</h3>	
-									</label>
-								</div>
-								<h1 className="pt-7">{data?.aboutplace?.place_price} INR</h1>
-
+								/>
+								<label htmlFor="">
+									<h3>Live</h3>
+								</label>
 							</div>
-							<h1 className="pt-7">{parseFloat(data?.totalPrice).toFixed(2)} INR</h1>
-						</div>
-
-						<div className="flex justify-between pt-4">
-							<div className="flex items-center space-x-1">
-							<Star ratings={data.ratings}/>
-								<span className="text-quicksilver text-xs font-bold pl-2">{data?.totalreview} ratings</span>
-							</div>
-							<div className="flex space-x-2">
-								<Link to={`../addplaces`} onClick={() => { localStorage.setItem("eventId", data?._id); localStorage.setItem("event_type", data?.event_type); dispatch(increment()) }} className="bg-brightGray px-2 py-1 text-center rounded"><i className="text-base edit text-black icon-edit" style={{ color: "#000" }}></i></Link>
-								<Link to="/" className="bg-brightGray px-2 py-1 text-center rounded"><i
-									className="icon-fill-megaphone text-base text-black"></i></Link>
-								<Link to={`/dashboard/event/calender`} className="bg-brightGray px-2 py-1 text-center rounded"><i
-									className="icon-calendar1 text-base text-black"></i></Link>
-								<Link to="/" className="bg-brightGray px-2 py-1 text-center rounded"><i
-									className="icon-percentage text-base text-black"></i></Link>
-								<Link to="/" className="bg-brightGray px-2 py-1 text-center rounded"><i
-									className="icon-share text-base text-black"></i></Link>
-							</div>
+							<h1 className="pt-7">{data?.aboutplace?.place_price} INR</h1>
 
 						</div>
+						<h1 className="pt-7">{parseFloat(data?.totalPrice).toFixed(2)} INR</h1>
+					</div>
+
+					<div className="flex justify-between pt-4">
+						<div className="flex items-center space-x-1">
+							<Star ratings={data.ratings} />
+							<span className="text-quicksilver text-xs font-bold pl-2">{data?.totalreview} ratings</span>
+						</div>
+						<div className="flex space-x-2">
+							<Link to={`../addplaces`} onClick={() => { localStorage.setItem("eventId", data?._id); localStorage.setItem("event_type", data?.event_type); dispatch(increment()) }} className="bg-brightGray px-2 py-1 text-center rounded"><i className="text-base edit text-black icon-edit" style={{ color: "#000" }}></i></Link>
+							<Link to="/" className="bg-brightGray px-2 py-1 text-center rounded"><i
+								className="icon-fill-megaphone text-base text-black"></i></Link>
+							<Link to={`/dashboard/event/calender`} className="bg-brightGray px-2 py-1 text-center rounded"><i
+								className="icon-calendar1 text-base text-black"></i></Link>
+							<Link to="/" className="bg-brightGray px-2 py-1 text-center rounded"><i
+								className="icon-percentage text-base text-black"></i></Link>
+							<Link to="/" className="bg-brightGray px-2 py-1 text-center rounded"><i
+								className="icon-share text-base text-black"></i></Link>
+						</div>
+
 					</div>
 				</div>
 			</div>
-
 		</div>
 	)
 }
